623ee1163b18e8862fc448435e987e9e609b1a43
Johanna Jato <johanna.jato@gmail.com>
2020-11-30 12:26:22 +0100

    ball

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 
 const zone = document.getElementById('zone');
-const snake = document.getElementById('snake');
-let move = setInterval(moveSnake, 500);
+const ctx = zone.getContext('2d');
+
+let move = setInterval(moveSnake, 100);
 let waistSnake = 10;
-let left = 50;
+let left = 300;
+let speed = 5;
+
+
+var ball = {
+  x: 100,
+  y: 100,
+  radius: 25,
+  color: 'blue',
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+};
+
+ball.draw()
 
 function moveSnake() {
-  if(left < 100) {
-    left += 1;
+  if(left < 600) {
+    left += speed;
+  } else {
+    left = 0;
   }
-  snake.style.left = (left - waistSnake) + "%";
+  snake.style.left = (left - waistSnake) + "px";
 
 
 }
