4855a4c0315404e30f33f60371618c141990556d
Johanna Jato <johanna.jato@gmail.com>
2020-11-30 11:34:16 +0100

    script

---
+++ script.js
@@ -0,0 +1,14 @@
+const zone = document.getElementById('zone');
+const snake = document.getElementById('snake');
+let move = setInterval(moveSnake, 500);
+let waistSnake = 10;
+let left = 50;
+
+function moveSnake() {
+  if(left < 100) {
+    left += 1;
+  }
+  snake.style.left = (left - waistSnake) + "%";
+
+
+}
