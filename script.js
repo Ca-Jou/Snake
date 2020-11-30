83fe78bddddba3a90f3b9e515492ea2250aceff5
Marco <marco.teixeiradasilva@hotmail.fr>
2020-11-30 11:31:52 +0100

    well done camille

---
+++ script.js
@@ -0,0 +1,8 @@
+let snake = document.getElementById('snake');
+let zone = document.getElementById('zone');
+let deplacement = 100;
+
+
+setTimeout(function() {
+    snake.style.left=deplacement
+}, 500);
